ecc910f0f1ff925fea1b011cc7b0422d07c1d49a
hoops <hoops@LAPTOP-18F7B063>
2023-10-05 00:09:30 +0800

    S5_Returning a list of items

--- MVC_NoSQL_FlowerSeed/FlowerSaleAPI/Controllers/ProductsController.cs
+++ MVC_NoSQL_FlowerSeed/FlowerSaleAPI/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ namespace FlowerSaleAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class ProductsController : ControllerBase
     {
         private readonly ShopContext _shopContext; //Automatically get the ShopContext injected
@@ -21,5 +22,19 @@ namespace FlowerSaleAPI.Controllers
         {
             return _shopContext.Products.ToArray();
         }
+
+        //public ActionResult GetAllProducts()
+        //{
+        //    return Ok(_shopContext.Products.ToArray());
+        //}
+        //[Route("api/[controller]")]
+        //[HttpGet]
+
+        //public ActionResult GetProduct(int id)
+        //{
+        //    var product = _shopContext.Products.Find(id);
+        //    //here use Ok product
+        //    return Ok(product);
+        //}
     }
 }
